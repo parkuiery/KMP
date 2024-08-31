5bbee0fa0d8a17dba3f4c5319ab00cff2649c18e
uiel <parkuiery@gmail.com>
2024-09-01 00:40:08 +0900

    feat :: sever dumy data

---
+++ Todos/server/src/main/kotlin/org/uiel/todos/dumy.kt
@@ -0,0 +1,33 @@
+package org.uiel.todos
+
+class dumy {
+    fun getData(): List<Todo> {
+        return listOf(
+            Todo(
+                id = 1,
+                title = "h",
+                content = "hh"
+            ),
+            Todo(
+                id = 2,
+                title = "ee",
+                content = "eee",
+            ),
+            Todo(
+                id = 3,
+                title = "rr",
+                content = "eee",
+            ),
+            Todo(
+                id = 4,
+                title = "ff",
+                content = "eee",
+            ),
+            Todo(
+                id = 5,
+                title = "aa",
+                content = "eee",
+            ),
+        )
+    }
+}
