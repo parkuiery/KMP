ab7f007970ec7c0041af68cf865e01af921f5539
uiel <parkuiery@gmail.com>
2024-09-01 00:41:28 +0900

    refactor :: change data

--- Todos/server/src/main/kotlin/org/uiel/todos/Application.kt
+++ Todos/server/src/main/kotlin/org/uiel/todos/Application.kt
@@ -1,20 +1,30 @@
 package org.uiel.todos
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = SERVER_PORT, host = "127.0.0.1", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
     routing {
         get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
+            call.respond(dumy().getData())
         }
     }
 }
