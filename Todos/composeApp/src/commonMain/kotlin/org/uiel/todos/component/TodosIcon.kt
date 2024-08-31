e27d83a765b5c28188722fcdeb863a16ed24a4a4
uiel <parkuiery@gmail.com>
2024-09-01 00:45:23 +0900

    feat :: icon

---
+++ Todos/composeApp/src/commonMain/kotlin/org/uiel/todos/component/TodosIcon.kt
@@ -0,0 +1,12 @@
+package org.uiel.todos.component
+
+import todos.composeapp.generated.resources.Res
+import todos.composeapp.generated.resources.baseline_add_24
+import todos.composeapp.generated.resources.baseline_arrow_back_24
+import todos.composeapp.generated.resources.baseline_delete_24
+
+object TodosIcon {
+    val Delete = Res.drawable.baseline_delete_24
+    val Add = Res.drawable.baseline_add_24
+    val Back = Res.drawable.baseline_arrow_back_24
+}
